8e01d170f360192299d871923549657efb67142f
isaac stott <isaac.cloyd@gmail.com>
2020-09-04 09:36:31 -0600

    completed ex1

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -4,4 +4,14 @@ def get_indices_of_item_weights(weights, length, limit):
     """
     # Your code here
 
+    weight_dict = {}
+
+    for i in range(len(weights)):
+        if limit - weights[i] in weight_dict:
+            return (i, weight_dict[limit - weights[i]])
+        weight_dict[weights[i]] = i
+
     return None
+
+weights = [12, 6, 7, 14, 19, 3, 0, 25, 40]
+print(get_indices_of_item_weights(weights, 9, 7))
