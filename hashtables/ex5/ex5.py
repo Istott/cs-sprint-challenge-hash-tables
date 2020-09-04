e698e804bfef227af894f982e27ffca8c52ca928
isaac stott <isaac.cloyd@gmail.com>
2020-09-04 11:30:39 -0600

    ex5. one test passing. one test failing.

--- hashtables/ex5/ex5.py
+++ hashtables/ex5/ex5.py
@@ -7,19 +7,49 @@ def finder(files, queries):
     YOUR CODE HERE
     """
     # Your code here
+    result = []
+    table = {}
+
+    for i in files:
+        filename = i.split("/")[-1]
+        if filename not in table:
+            table[filename] = i
+        # table[filename].append(i)
+    
+    for i in queries:
+        if i in table:
+            result.append(table[i])
+
+
+    result.sort()
 
     return result
 
 
+# if __name__ == "__main__":
+#     files = [
+#         '/bin/foo',
+#         '/bin/bar',
+#         '/usr/bin/baz'
+#     ]
+#     queries = [
+#         "foo",
+#         "qux",
+#         "baz"
+#     ]
+#     print(finder(files, queries))
+
+
 if __name__ == "__main__":
-    files = [
-        '/bin/foo',
-        '/bin/bar',
-        '/usr/bin/baz'
-    ]
+    files = ['/dir256/dirb256/file256',
+            '/dir256/file256', '/dir3490/dirb3490/file3490',
+            '/dir3490/file3490', '/dir8192/dirb8192/file8192',
+            '/dir8192/file8192']
+
     queries = [
-        "foo",
-        "qux",
-        "baz"
-    ]
+            "file3490",
+            "file256",
+            "file999999",
+            "file8192"
+        ]
     print(finder(files, queries))
