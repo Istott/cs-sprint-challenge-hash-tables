a819ec50ab7e47099f1bb1e8670a3bc512709866
isaac stott <isaac.cloyd@gmail.com>
2020-09-04 10:22:46 -0600

    completed ex2

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -13,5 +13,9 @@ def get_indices_of_item_weights(weights, length, limit):
 
     return None
 
-weights = [12, 6, 7, 14, 19, 3, 0, 25, 40]
-print(get_indices_of_item_weights(weights, 9, 7))
+
+weights_3 = [4, 6, 10, 15, 16]
+print(get_indices_of_item_weights(weights_3, 5, 21))
+
+weights_4 = [12, 6, 7, 14, 19, 3, 0, 25, 40]
+print(get_indices_of_item_weights(weights_4, 9, 7))
